95ba1dd367a191b66b73d4b5d4245d91ef480e66
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-02 16:38:38 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -87,20 +87,20 @@ def page_dashboard():
              return
 
     # Définir la colonne 'DATE' comme index
-    Evol_df.set_index('DATE', inplace=True)
+          Evol_df.set_index('DATE', inplace=True)
 
     # Grouper les données mensuellement
-    monthly_data_grouped = Evol_df.resample('M').mean()
+           monthly_data_grouped = Evol_df.resample('M').mean()
 
     # Créer un graphique Plotly Express
-    fig1 = px.line(monthly_data_grouped, x=monthly_data_grouped.index, y='VOLUME', title='Monthly Evolution', markers=True)
-    fig1.update_traces(texttemplate='%{y:.2f}', textposition='top center', mode='markers+lines+text')
-    fig1.update_xaxes(
+          fig1 = px.line(monthly_data_grouped, x=monthly_data_grouped.index, y='VOLUME', title='Monthly Evolution', markers=True)
+          fig1.update_traces(texttemplate='%{y:.2f}', textposition='top center', mode='markers+lines+text')
+          fig1.update_xaxes(
         dtick='M1',  # Marquer tous les mois
-        tickformat='%b %Y',  # Format de l'étiquette (abrégé du mois et année)
-        tickangle=45,  # Angle de rotation des étiquettes (facultatif)
+          tickformat='%b %Y',  # Format de l'étiquette (abrégé du mois et année)
+          tickangle=45,  # Angle de rotation des étiquettes (facultatif)
     )
-    fig1.update_layout(width=700, height=500, bargap=0.1, plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
+          fig1.update_layout(width=700, height=500, bargap=0.1, plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)')
 
     # Afficher le graphique dans l'interface Streamlit
     st.plotly_chart(fig1)

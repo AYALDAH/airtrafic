166beb6cf008754f8c1f645cd4907f873324e109
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-02 16:21:52 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -82,6 +82,7 @@ def page_dashboard():
     if  Analyse_Exploratoire == 'Analyse_mensuelle': 
         plt.figure(figsize=(12,14)) 
         #Evol_df['DATE'] = pd.to_datetime(Evol_df['DATE'], format='%Y-%m-%d')
+        Evol_df.set_index('DATE', inplace=True)
         monthly_data_grouped =Evol_df.resample('M', on='DATE').mean()
         fig1 = px.line(monthly_data_grouped, x=monthly_data_grouped.index, y='VOLUME', title='Monthly Evolution', markers=True)
         fig1.update_traces(texttemplate='%{y:.2f}', textposition='top center', mode='markers+lines+text')

3be7371470d1471b71d31e6569c2fae9deb78d49
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 16:32:15 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -204,7 +204,7 @@ def page_dashboard():
         monthly_data_grouped_sorted = monthly_data_grouped.sort_values(by='VOLUME', ascending=False)
 
 # Sélectionner les N premières lignes pour obtenir les sites et mois avec les volumes les plus élevés
-        monthly_data_grouped3 = monthly_data_grouped_sorted.nlargest(N, 'VOLUME')
+        monthly_data_grouped3 = monthly_data_grouped_sorted.nlargest(3, 'VOLUME')
         top_months3 = monthly_data_grouped3.head(3)
         top_months3['Month'] = top_months3['Month'].map(mois_fr)
         st.plotly_chart(fig3)

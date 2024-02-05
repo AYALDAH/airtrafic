b20cc77c6378ed34710b33d6afd1b20377e2ae18
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 17:25:16 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -207,7 +207,7 @@ def page_dashboard():
         top_months3= monthly_data_grouped3.head(3)
         top_months3['Month'] = top_months3['Month'].map(mois_fr)
         st.plotly_chart(fig3)
-        st.write('Au cours des mois de ', 'et '.join(top_months3['Month']), 'les sites  enrégistrent les volumes les plus élévé'  )
+        st.write('Au cours des mois de ', ', ','et'.join(top_months3['Month']), 'les sites  enrégistrent les volumes les plus élévé'  )
                  
 # Créez une barre latérale pour la navigation entre les pages
 page = st.sidebar.radio("Visualisation", ["Resumé","Analyse Exploratoire", "Techniques de Machine Learning"])

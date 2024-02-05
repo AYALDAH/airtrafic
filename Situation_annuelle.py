5c694074182ab23dc81526a95f84e716cea04481
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 16:55:12 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -203,9 +203,9 @@ def page_dashboard():
 # Trier le DataFrame par volume décroissant
         monthly_data_grouped3= monthly_data_grouped.sort_values(by='VOLUME', ascending=False)
         top_months3= monthly_data_grouped3.head(3)
-        top_months3['Month'] = top_months3['Month']
+        top_months3['Month'] = top_months3['Month'].map(mois_fr)
         st.plotly_chart(fig3)
-        st.write('Au cours des mois de ', ', '.join(top_months3), 'les sites  enrégistrent les volumes les plus élévé'  )
+        st.write('Au cours des mois de ', ', '.join(top_months3['Month']), 'les sites  enrégistrent les volumes les plus élévé'  )
                  
 # Créez une barre latérale pour la navigation entre les pages
 page = st.sidebar.radio("Visualisation", ["Resumé","Analyse Exploratoire", "Techniques de Machine Learning"])

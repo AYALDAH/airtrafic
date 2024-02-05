8b640180907fe4593d64c782d173154716f51a00
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 17:34:45 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -210,7 +210,7 @@ def page_dashboard():
         # Trier le DataFrame par entité décroissant 
         top_months4= top_months3.sort_values(by='ENTITE', ascending=False)
         top_months4= monthly_data_grouped3.head(1)
-        st.write('Au cours des mois de ', ', '.join(top_months3['Month']), 'les sites Dde', ', '.join(top_months3['ENTITE']) enrégistrent les volumes les plus élévé'  )
+        st.write('Au cours des mois de ', ', '.join(top_months3['Month']), 'les sites de, ', '.join(top_months3['ENTITE']) enrégistrent les volumes les plus élévé'  )
 
 
 # Créez une barre latérale pour la navigation entre les pages

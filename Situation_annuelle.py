b26b29beab2607443b915e62836e8f56bfc107b2
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 12:03:04 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -115,7 +115,17 @@ def page_dashboard():
         col1, col2 = st.columns(2)
         with col1:
             st.plotly_chart(fig1)
-            st.write('Des pics de volumes moyens sont constatés aux mois de :' )
+    # Convertir la colonne 'DATE' en format de mois
+             analyse_df['Month'] = analyse_df['DATE'].dt.strftime('%B')
+
+    # Trier le DataFrame par volume décroissant
+              analyse_df = analyse_df.sort_values(by='VOLUME', ascending=False)
+
+    # Sélectionner les mois avec les volumes les plus élevés (par exemple, les 5 premiers mois)
+               top_months = analyse_df.head(3)
+
+# Afficher les résultats
+               st.write('Des pics de volumes moyens sont constatés aux mois de :', ', '.join(top_months['Month']))
         with col2:
             st.plotly_chart(fig2)
 

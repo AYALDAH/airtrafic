ef734f30ebe34dc60ca5f1745248932d2b5e887b
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-05 11:30:48 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -13,6 +13,7 @@ import xgboost as xgb
 from mlforecast import MLForecast
 import numba
 from numba import njit
+from sklearn.impute import KNNImputer
 from sklearn.ensemble import RandomForestRegressor
 from window_ops.expanding import expanding_mean
 from window_ops.rolling import rolling_mean

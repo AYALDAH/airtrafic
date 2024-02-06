8db231d9c8ebfbdb0fa16d9e178fad2d4e90bebb
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-06 10:49:47 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -226,7 +226,7 @@ def page_dashboard():
             top_entities.append(top_entity_in_month)
 
        
-        st.write('Au cours des mois de', ', '.join(top_months['Month']), 'les sites de', ', '.join(top_entities['ENTITE']), 'enregistrent les volumes les plus élevés')
+        st.write('Au cours des mois de', ', '.join(top_months['Month']), 'les sites de', ', '.join(), 'enregistrent les volumes les plus élevés')
 
 
 # Créez une barre latérale pour la navigation entre les pages

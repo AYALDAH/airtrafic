312f06eb90143651662976e1dc89c09263a61e7c
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-06 12:11:14 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -227,7 +227,7 @@ def page_dashboard():
       
         for i, (month_index, _) in enumerate(top_months.iterrows()):
                                  entities_str = ', '.join(top_entities[i].index.tolist())
-        st.write(f"Pour le mois {month_index.strftime('%B')} les entités avec le plus de volume sont : {entities_str}")
+                                 st.write(f"Pour le mois {month_index.strftime('%B')} les entités avec le plus de volume sont : {entities_str}")
             
        # st.write('Au cours des mois de', ', '.join(top_months['Month'], 'les sites de', ', '.join(top_entities['ENTITE']), 'enregistrent les volumes les plus élevés'))
 

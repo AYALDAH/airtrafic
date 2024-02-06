7365ec72030ccd58170b81287a1bf6796d546fff
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-06 11:33:51 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -224,9 +224,8 @@ def page_dashboard():
             top_entity_in_month = entities_volume.head(3)
 # Ajouter l'entité à la liste des entités ayant le plus de volume parmi les mois ayant le plus de volume
             top_entities.append(top_entity_in_month)
-
-          for i, (month_index, _) in enumerate(top_months.iterrows()):
-              st.write(f"Pour le mois {month_index.strftime('%B')} les entités avec le plus de volume sont : {', '.join(top_entities[i].index)}")
+         for i, (month_index, _) in enumerate(top_months.iterrows()):
+             st.write(f"Pour le mois {month_index.strftime('%B')} les entités avec le plus de volume sont : {', '.join(top_entities[i].index)}")
        # st.write('Au cours des mois de', ', '.join(top_months['Month'], 'les sites de', ', '.join(top_entities['ENTITE']), 'enregistrent les volumes les plus élevés'))
 
 # Créez une barre latérale pour la navigation entre les pages

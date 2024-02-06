64f42389c4e0db499ef3b8584b73da6461149049
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-06 10:28:09 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -208,12 +208,6 @@ def page_dashboard():
     st.plotly_chart(fig3)  
     
 # Trier le DataFrame par volume décroissant
-         monthly_data_groupedP = Evol_df.resample('M',on='DATE').mean()
-         monthly_data_groupedP['Month'] = monthly_data_groupedP.index.strftime('%B')
-         monthly_data_grouped3= monthly_data_groupedP.sort_values(by='VOLUME', ascending=False)
-         top_months3= monthly_data_grouped3.head(3)
-         top_months3['ENTITE']=Evol_df['ENTITE']
-         top_months3['Month'] = top_months3['Month'].map(mois_fr)
         # Trier le DataFrame par entité décroissant 
          top_entities = [] 
          monthly_data_groupedP = Evol_df.resample('M',on='DATE').mean()

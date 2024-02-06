a73b88f3485b4e1fe088c1388e6bcb47ef753e6e
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-02-06 15:03:34 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -223,13 +223,12 @@ def page_dashboard():
             top_entities_in_month = entities_volume.nlargest(3)
     # Ajouter les entités à la liste des entités ayant le plus de volume parmi les mois ayant le plus de volume
             top_entities.append(top_entities_in_month)     
-        all_entities = set()
-        for entities_series in top_entities:
-            all_entities.update(entities_series.index.tolist())
-# Afficher les entités avec le plus de volume pour chaque mois
-        for i, (month_index, _) in enumerate(top_months.iterrows()):
-            st.write(f"Pour le mois de {month_index.strftime('%B')}, les entités avec le plus de volume sont : {', '.join(all_entities)}")
-           
+         for i, (month_index, _) in enumerate(top_months.iterrows()):
+             entities_series = top_entities[i]
+             if isinstance(entities_series, pd.Series) and isinstance(entities_series.index, pd.Index):
+                 st.write(f"Pour le mois de {month_index.strftime('%B')}, les entités avec le plus de volume sont : {', '.join(entities_series.index.tolist())}")
+             else:
+                 st.write(f"Erreur: Les données pour le mois de {month_index.strftime('%B')} ne sont pas disponibles.")
 # Créez une barre latérale pour la navigation entre les pages
 page = st.sidebar.radio("Visualisation", ["Resumé","Analyse Exploratoire", "Techniques de Machine Learning"])
 # Affichage conditionnel en fonction de la page sélectionnée

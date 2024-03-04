124917b0740adf6480c96755fe7988d84a6ac6f7
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-04 15:59:11 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -244,7 +244,7 @@ def page_dashboard():
         monthly_data_grouped['Change'] = monthly_data_grouped['VOLUME'].diff().fillna(0)
 
 # Create the waterfall chart using Plotly Express
-        fig_waterfall = px.bar(monthly_data_grouped, x='DATE', y='Change', title='Variation du volume moyen en 2022', barmode='overlay', labels={'DATE_RENTA': 'Date', 'Change': 'Change in Volume'},color_discrete_sequence=px.colors.qualitative.Plotly)
+        fig_waterfall = px.bar(monthly_data_grouped, x='DATE', y='Change', title='Variation du volume moyen en 2022', barmode='overlay', labels={'DATE': 'Date', 'Change': 'Change in Volume'},color_discrete_sequence=px.colors.qualitative.Plotly)
 
 # Update layout and appearance of the plot
         fig_waterfall.update_layout(height=400, width=800)

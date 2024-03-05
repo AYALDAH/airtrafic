18285e12dfa143fef651cfeb2903f221de9a3564
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-05 15:48:52 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -259,7 +259,8 @@ def page_dashboard():
 #Commentaires
             monthly_data_grouped = filtered_data.groupby([pd.Grouper(key='DATE', freq='M')])['VOLUME'].sum().reset_index()
             monthly_data_grouped = monthly_data_grouped.resample('M',on='DATE').mean()
-            monthly_data_grouped=monthly_data_grouped.sort_values(by='VOLUME', ascending=True)
+            monthly_data_grouped['Change'] = monthly_data_grouped['VOLUME'].diff().fillna(0)
+            monthly_data_grouped=monthly_data_grouped.sort_values(by='Change', ascending=True)
             monthly_data_grouped['Month'] = monthly_data_grouped.index.strftime('%B')
             top_months = monthly_data_grouped.head(3)
             top_months['Month'] = top_months['Month'].map(mois_fr)

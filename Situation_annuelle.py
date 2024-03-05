9b5259e40e0cf5a2281ec406b4ecc01d141fe4e8
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-05 15:30:11 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -258,7 +258,7 @@ def page_dashboard():
             st.plotly_chart(fig_waterfall)
 #Commentaires
             monthly_data_grouped = filtered_data.groupby([pd.Grouper(key='DATE', freq='M')])['VOLUME'].sum().reset_index()
-            monthly_data_grouped = Evol_df.resample('M',on='DATE').mean()
+            monthly_data_grouped = monthly_data_grouped.resample('M',on='DATE').mean()
             monthly_data_grouped=monthly_data_grouped.sort_values(by='VOLUME', ascending=True)
             monthly_data_grouped['Month'] = monthly_data_grouped.index.strftime('%B')
             top_months = monthly_data_grouped.head(3)

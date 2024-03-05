0b1723a766d79d8bd3f948704f12c146c5591f9c
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-05 14:55:30 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -258,9 +258,9 @@ def page_dashboard():
             st.plotly_chart(fig_waterfall)
 #Commentaires
             monthly_data_grouped=monthly_data_grouped.sort_values(by='VOLUME', ascending=True)
-            low_months = monthly_data_grouped.top(3)
-            low_months['Month'] = low_months['Month'].map(mois_fr)
-            st.write('Sur le site de '.join(selected_entity), 'les baisses les plus importantes de volume ont lieu en ',' '.join(low_months['Month']))
+            top_months = monthly_data_grouped.top(3)
+            top_months['Month'] = top_months['Month'].map(mois_fr)
+            st.write('Sur le site de '.join(selected_entity), 'les baisses les plus importantes de volume ont lieu en ',' '.join(top_months['Month']))
         #Indicateur MONTANT
         if st.sidebar.button("MONTANT"):
             filtered_data = Evol_df[Evol_df['ENTITE'] == selected_entity]

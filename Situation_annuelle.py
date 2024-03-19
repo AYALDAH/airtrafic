1b4742021cc6673c532e59f974b1b428fd958e17
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-19 15:19:51 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -481,7 +481,7 @@ def page_dashboard():
                        marker=dict(colors=colors, line=dict(color='white', width=0)),
                        textinfo='percent+label', hole=0.3, sort=False,
                        pull=explode, textfont_size=12))  # Decrease the font size to 12
-                       fig_sens=fig_sens.update_layout( width=300, height=350)
+                       fig_sens=fig_sens.update_layout( width=500, height=400)
 
              #Présentation en colonne
                        col7, col8 = st.columns(2)

12ff6ab57d4730e4253bc8987ae45a267a20ffb6
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-19 12:24:15 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -451,9 +451,9 @@ def page_dashboard():
                        treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="ACTIVITE PRINCIPALE")
                        treemap1=treemap1.update_layout( width=400, height=500)
                       #Pays du client
-                       sector_counts = filtered_data['PAYS_CLIENT'].value_counts()
-                       top_countries = sector_counts.nlargest(5).index
-                       filtered_data_top = top_countries[top_countries['PAYS_CLIENT'].isin(top_countries)]
+                       country_counts= filtered_data['PAYS_CLIENT'].value_counts()
+                       top_countries = country_counts.nlargest(5).index
+                       filtered_data_top = filtered_data[filtered_data['PAYS_CLIENT'].isin(top_countries)]
                        pays_bar = px.bar(filtered_data_top, x='PAYS_CLIENT', y=filtered_data_top.index, orientation='h')
                        pays_bar.update_layout(title = dict(text = "Graphique du pourcentage par Entité"))
                        pays_bar.update_layout(title='PAYS_CLIENT', plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,1,1,0)', width=600, height=400, xaxis=dict(title="count"),  # Add x-axis label

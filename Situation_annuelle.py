5fcf7a2cecac25adddabc37421b3cc9660e08fef
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-20 12:11:14 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -543,7 +543,7 @@ def page_dashboard():
 
                 #Site de Rouen
                     
-                if st.sidebar.button("ROUEN"):
+                elif st.sidebar.button("ROUEN"):
                         st.write("**Les statistiques du site de Rouen**")
                         filtered_data = Maritime_df[Maritime_df['ENTITE'] == "ROUEN"]
                       

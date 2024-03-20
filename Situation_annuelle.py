7d108158fc2246f700b8e40649b90269c664d067
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-20 09:44:14 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -490,7 +490,8 @@ def page_dashboard():
                        with col8:
                            st.plotly_chart(treemap2)
             #Cloroplètre
-                       px.choropleth(filtered_data, locations="code_iso_d", hover_name="PAYS_DEPART_LO", color="MARGE")
+                     Chloroplètre= px.choropleth(filtered_data, locations="code_iso_d", hover_name="PAYS_DEPART_LO", color="MARGE")
+                     st.plotly_chart(fig_sens)
 def page_ML():
     st.title("")
     col1, col2 = st.columns([1, 5])

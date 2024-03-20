ba57ad7b008bfb9d9bf11c9d5d2d24a43f30267a
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-20 16:27:11 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -526,14 +526,14 @@ def page_dashboard():
         #Les TEU
                        # Update layout and appearance of the plot
                        #Type TEU
-            colors = ['deepskyblue', 'orange','red']
+            colors = ['deepskyblue', 'salmon']
             explode = [0.1, 0]
             d_2 = pd.DataFrame(filtered_data["TYPE_DOSSIER"].value_counts())
             fig7 = go.Figure()
             fig7.add_trace(go.Pie(labels=d_2.index, values=d_2['TYPE_DOSSIER'],marker=dict(colors=colors, line=dict(color='white', width=0)),textinfo='percent+label', hole=0.3, sort=False,pull=explode, textfont_size=12))  # Decrease the font size to 12
             fig7=fig7.update_layout(title=dict(text=" Type de Dossier, Nombre et Taille du TEU"),plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,1,1,0)',showlegend=False,width=310, height=400,
             xaxis=dict(showline=False, showgrid=False),yaxis=dict(showline=False, showgrid=False),annotations=[dict(text='type', x=0.50, y=0.45, font_size=20, showarrow=False)] )
-            colors = ['deepskyblue', 'salmon','lightgreen']
+            colors = ['deepskyblue', 'salmon','khaki']
             explode = [0.1, 0]
                     #Nombre de teu  et taille
             fig8 = go.Figure()

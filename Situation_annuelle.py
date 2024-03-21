9e8c0ad9c3123fde2fa54454f5d824344cb63cde
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-21 11:52:31 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -197,7 +197,7 @@ def page_dashboard():
 # Création du graphique à barres empilées avec des couleurs personnalisées (Volumes par mois et par Sites)
         fig3 = go.Figure()
         #colors = ['chocolate','Peru','darkorange','deepskyblue','silver','lightyellow']  # Liste de couleurs personnalisées
-        Colors=px.colors.sequential.Jet
+        Colors=px.colors.sequential.ice
         for i, entity in enumerate(monthly_data_grouped['ENTITE'].unique()):
               entity_data = monthly_data_grouped[monthly_data_grouped['ENTITE'] == entity]
               fig3.add_trace(go.Bar(

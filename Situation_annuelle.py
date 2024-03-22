b9de4d0cd70fefa364bb0392ff36b17d5dc5c6e1
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-22 12:42:29 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -489,7 +489,7 @@ def page_dashboard():
             sector_counts = filtered_data['SECTEUR_ACTIVITE_PRINCIPAL'].value_counts()
             top_sectors = sector_counts[sector_counts >10].index
             filtered_data_top = filtered_data[filtered_data['SECTEUR_ACTIVITE_PRINCIPAL'].isin(top_sectors)]
-            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="",color_discrete_sequence = 'RdBu')
+            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="")
             treemap1=treemap1.update_layout( width=400, height=450)
                       
         #Pays du client

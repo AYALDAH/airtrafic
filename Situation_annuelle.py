402e0128ff82a02efe95b1b0841071051a6893b9
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-22 12:08:59 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -490,7 +490,7 @@ def page_dashboard():
             top_sectors = sector_counts[sector_counts >10].index
             filtered_data_top = filtered_data[filtered_data['SECTEUR_ACTIVITE_PRINCIPAL'].isin(top_sectors)]
             colors = ["#ff7f0e", "#1f77b4", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="";color_continuous_scale=colors)
+            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="",color_continuous_scale=colors)
             treemap1=treemap1.update_layout( width=400, height=450)
                       
         #Pays du client
@@ -513,7 +513,7 @@ def page_dashboard():
             top_sectors = sector_counts[sector_counts > 10].index
             filtered_data_top = filtered_data[filtered_data['ARMATEUR'].isin(top_sectors)]
             colors = ["#ff7f0e", "#1f77b4", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-            treemap2= px.treemap(filtered_data_top,path=["ARMATEUR"],title="";color_continuous_scale=colors)
+            treemap2= px.treemap(filtered_data_top,path=["ARMATEUR"],title="",color_continuous_scale=colors)
             treemap2=treemap2.update_layout( width=400, height=450)
 
         #sens

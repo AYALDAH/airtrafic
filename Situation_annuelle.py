6ded8ab10533879879844845608c7c0f26f81550
AYALDAH <118635847+AYALDAH@users.noreply.github.com>
2024-03-22 13:43:31 +0100

    Update Situation_annuelle.py

--- Situation_annuelle.py
+++ Situation_annuelle.py
@@ -489,7 +489,8 @@ def page_dashboard():
             sector_counts = filtered_data['SECTEUR_ACTIVITE_PRINCIPAL'].value_counts()
             top_sectors = sector_counts[sector_counts >10].index
             filtered_data_top = filtered_data[filtered_data['SECTEUR_ACTIVITE_PRINCIPAL'].isin(top_sectors)]
-            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="")
+            color=custom_color_scale_without_green = ["#ff7f0e", "#1f77b4", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+            treemap1= px.treemap(filtered_data_top,path=["SECTEUR_ACTIVITE_PRINCIPAL"],title="",color_discrete_sequence=color)
             treemap1=treemap1.update_layout( width=400, height=450)
                       
         #Pays du client
